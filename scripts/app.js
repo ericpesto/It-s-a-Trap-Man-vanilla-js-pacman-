4502992a8d3314ca4ab4dd82f2671ca411552f02
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-02-19 13:42:28 +0000

    maze begun

--- scripts/app.js
+++ scripts/app.js
@@ -6,13 +6,17 @@ function init() {
   const width = 20
   const cellCount = width * width
   const cells = []
+  
 
   const playerClass = 'player'
   const playerStartPosition = 369
   let playerCurrentPosition = 369
 
-  // const terrainClass = 'terrain'
-  // const terrainArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,29,30,39]
+  const mazeClass = 'maze'
+  const mazeArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,29,30,39,40,42,43,44,45,46,47,49,50,52,53,54,55,56,57,59,60,62,63,64,65,66,67,69,70,72,73,74,75,76,77,79,80,89,90,99,100,102,103,104,105,106,113,114,115,116,117,119,120,122,123,124,125,126,128,131,133,134,135,136,137,139,140,148,151,159]
+
+
+  //console.log('mazeArray ->', mazeArray)
 
 
   // * Make Grid
@@ -21,9 +25,22 @@ function init() {
       const cell = document.createElement('div')
       cell.textContent = i
       grid.appendChild(cell)
+      cell.id = i
       cells.push(cell)
+      //need to compare cell.id to mazeArray values and add the maze class when true.
+      console.log('cell.id ->', parseFloat(cell.id))
+
+      if (mazeArray.includes(Number(cell.id))) {
+        console.log('match')
+        cell.classList.add(mazeClass)
+      }
     }
+    
+
+
+
     addPlayer(playerStartPosition)
+    //createMaze()
   }
   // * Add player to grid
   function addPlayer(position) {
@@ -36,8 +53,10 @@ function init() {
   }
 
   // * Obstacles, assign by class 1) grid out grid items to be 'obstacles' and assign a class to those.
-  // function handleObstacles() {
-
+  // function createMaze(cells) { 
+  //   mazeArray.forEach(block => {
+  //     if ()
+  //   })
   // }
 
 
