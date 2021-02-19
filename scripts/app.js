8c57e8bbb00a0e75cc716c2c645d9cef515dbdbe
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-02-19 22:13:11 +0000

    push

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,14 @@ function init() {
   const cellCount = width * width
   const cells = []
   const mazeClass = 'maze-wall'
-  const mazeArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,29,30,39,40,42,43,45,46,47,49,50,52,53,54,56,57,59,60,62,63,65,66,67,69,70,72,73,74,76,77,79,80,87,89,90,92,99,100,102,103,105,114,116,117,119,120,122,123,125,126,128,131,133,134,136,137,139,140,148,151,159,160,161,162,163,164,165,166,168,169,170,171,173,174,175,176,177,178,179,200,201,202,203,204,206,208,209,210,211,213,215,216,217,218,219,220,226,233,239,240,242,243,244,246,247,248,249,250,251,252,253,255,256,257,259,260,262,277,279,280,284,286,288,289,290,291,293,295,279,299,300,302,303,304,306,313,315,316,317,319,320,326,327,328,329,330,331,332,333,339,340,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,359,360,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399]
+  const mazeArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,29,30,39,40,42,43,45,46,47,49,50,52,53,54,56,57,59,60,62,63,65,66,67,69,70,72,73,74,76,77,79,80,87,89,90,92,99,100,102,103,105,114,116,117,119,120,122,123,125,126,128,131,133,134,136,137,139,140,148,151,159,160,161,162,163,164,165,166,168,169,170,171,173,174,175,176,177,178,179,200,201,202,203,204,206,208,209,210,211,213,215,216,217,218,219,220,226,233,239,240,242,243,244,246,247,248,249,250,251,252,253,255,256,257,259,260,262,277,279,280,284,286,288,289,290,291,293,295,279,299,300,302,303,304,306,313,315,316,317,319,320,326,327,328,329,330,331,332,333,339,340,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,359,360,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399] 
+  const ghostHomeArray = [129,130,149,150]
+  const mergedMazeAndGhostHomeArray = mazeArray.concat(ghostHomeArray)
+
+  const pelletClass = 'pellet'
+  const superPelletClass = 'super-pellet'
+  const pelletArray = []
+
 
   // * Player Variables
   const playerClass = 'player'
@@ -18,12 +25,7 @@ function init() {
   // * Game state/logic variables
   // let score = 0
   // let lives = 3
-  
-  // ? const ghostHome = [129,130,149,150]
 
-  const pelletClass = 'pellet'
-  const superPelletClass = 'super-pellet'
-  const pelletArray = []
 
 
   // * Make Grid
@@ -41,7 +43,7 @@ function init() {
       }
 
       // * Render pellets with css 
-      if (!mazeArray.includes(Number(cell.id))) {
+      if (!mergedMazeAndGhostHomeArray.includes(Number(cell.id))) {
         pelletArray.push(cell)
         createPellets()
       }
@@ -82,12 +84,16 @@ function init() {
     // * player movement logic
     if (key === 39) {
       playerDirection = 'right'
+      console.log('Moving right')
     } else if (key === 37) {
       playerDirection = 'left'
+      console.log('Moving left')
     } else if (key === 38) {
       playerDirection = 'up'
+      console.log('Moving up')
     } else if (key === 40) {
       playerDirection = 'down'
+      console.log('Moving down')
     } else {
       console.log('invalid key')
     }
@@ -104,18 +110,14 @@ function init() {
 
     removePlayer(playerCurrentPosition)
 
-    if (playerDirection === 'right' && playerCurrentPosition % width !== width - 1 && !mazeArray.includes(playerRelativePositionRight)) {
+    if (playerDirection === 'right' && playerCurrentPosition % width !== width - 1 && !mergedMazeAndGhostHomeArray.includes(playerRelativePositionRight)) {
       playerCurrentPosition++
-      console.log('Moving right')
-    } else if (playerDirection === 'left' && playerCurrentPosition % width !== 0 && !mazeArray.includes(playerRelativePositionLeft)) {
+    } else if (playerDirection === 'left' && playerCurrentPosition % width !== 0 && !mergedMazeAndGhostHomeArray.includes(playerRelativePositionLeft)) {
       playerCurrentPosition--
-      console.log('Moving left')
-    } else if (playerDirection === 'up' && playerCurrentPosition >= width && !mazeArray.includes(playerRelativePositionUp)) {
+    } else if (playerDirection === 'up' && playerCurrentPosition >= width && !mergedMazeAndGhostHomeArray.includes(playerRelativePositionUp)) {
       playerCurrentPosition -= width
-      console.log('Moving up')
-    } else if (playerDirection === 'down' && playerCurrentPosition + width <= width * width - 1 && !mazeArray.includes(playerRelativePositionDown)) {
+    } else if (playerDirection === 'down' && playerCurrentPosition + width <= width * width - 1 && !mergedMazeAndGhostHomeArray.includes(playerRelativePositionDown)) {
       playerCurrentPosition += width
-      console.log('Moving down')
     } else {
       console.log('Ouch! Wall!')
     }
@@ -140,9 +142,6 @@ function init() {
         //pelletArray.classList.remove(pelletClass)
       }
     })
-    
-
-    
   }
 
 
