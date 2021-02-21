6103afff0db0b1ca6a02d1332ae7e951b27c1f54
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-02-21 01:45:57 +0000

    score bug breakthrough, solution close

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,7 @@ function init() {
   const width = 20
   const cellCount = width * width
   const cells = []
+  let cell 
   const mazeClass = 'maze-wall'
   const mazeArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,29,30,39,40,42,43,45,46,47,49,50,52,53,54,56,57,59,60,62,63,65,66,67,69,70,72,73,74,76,77,79,80,87,89,90,92,99,100,102,103,105,114,116,117,119,120,122,123,125,126,128,131,133,134,136,137,139,140,148,151,159,160,161,162,163,164,165,166,168,169,170,171,173,174,175,176,177,178,179,200,201,202,203,204,206,208,209,210,211,213,215,216,217,218,219,220,226,233,239,240,242,243,244,246,247,248,249,250,251,252,253,255,256,257,259,260,262,277,279,280,284,286,288,289,290,291,293,295,279,299,300,302,303,304,306,313,315,316,317,319,320,326,327,328,329,330,331,332,333,339,340,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,359,360,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399] 
   const ghostHomeArray = [129,130,149,150]
@@ -27,8 +28,9 @@ function init() {
   let playerDirection = 'right'
 
   // * Game state/logic variables
-  let score = 0
+  const score = 0
   // let lives = 3
+  // let scoreArray = 0
   
 
   // * Ghosts
@@ -36,6 +38,9 @@ function init() {
   const char = {
     name: 'Char',
     className: 'char',
+    startingPosition: 21,
+    currentPosition: 21,
+    targetPosition: 229,
     chase() {
       // targets a target tile is clculated everytime before a decsiion to move is made
       // each ghost has uniqe behaviour/target tile based on player position
@@ -51,44 +56,35 @@ function init() {
     eaten() {
       // ghost targets ghost home/starting position
       // once home they revert to scatter or chase mode
+    },
+    add() {
+
+    },
+    remove() {
+
     }
   }
-
 
   // * Make Grid
   function createGrid(playerStartPosition) {
     for (let i = 0; i < cellCount; i++) {
-      const cell = document.createElement('div')
-      //cell.textContent = i
+      cell = document.createElement('div')
+      cell.textContent = i
       grid.appendChild(cell)
       cell.id = i
       //cell.classList.add('grid-item')
       cells.push(cell)
       
-      // * Render maze with css
-      // if (mazeArray.includes(Number(cell.id))) {
-      //   cell.classList.add(mazeClass)
-      // }
       renderMaze(cell)
-
-      // * Render ghostHome with css
-      // if (ghostHomeArray.includes(Number(cell.id))) {
-      //   cell.classList.add(ghostHomeClass)
-      // }
       renderGhostHome(cell)
-
-      // * Render pellets with css 
-      // if (!mergedMazeAndGhostHomeArray.includes(Number(cell.id))) {
-      //   pelletArray.push(cell)
-      //   createPellets(pelletArray)
-      // }
       renderPellets(cell)
     }
-    
+    //console.log(cells)
     addPlayer(playerStartPosition)
-    //will add ghosts here too
+    // will add ghosts initial position here too
   }
-
+  
+  
   function renderMaze(cell) {
     if (mazeArray.includes(Number(cell.id))) {
       cell.classList.add(mazeClass)
@@ -104,40 +100,20 @@ function init() {
   function renderPellets(cell) {
     if (!mergedMazeAndGhostHomeArray.includes(Number(cell.id))) {
       pelletArray.push(cell)
-      createPellets(pelletArray)
+      addPellets(pelletArray)
     }
   }
 
   // * Create pellets and super pellets
-  // ! CSS APPROACH NOT WORKING FOR SCORE, but html approach fucked up the grid
-  function createPellets() {
+  // ! CSS APPROACH NOT WORKING FOR SCORE, html approach messed up the grid
+  function addPellets() {
     pelletArray.forEach(pellet => {
       if (pellet.id % 28 === 0) {
         pellet.classList.add(superPelletClass)
-        pellet.setAttribute('data-score', 50)
+        //pellet.setAttribute('data-score', 50)
       } else {
         pellet.classList.add(pelletClass)
-        pellet.setAttribute('data-score', 10)
-      }
-    })
-  }
- 
-  // ! Remove pellets WIP, pellets are removed visually atm
-  function removePellet(position) {
-    pelletArray.forEach(pellet => {
-      if (position === Number(pellet.id)) {
-        //console.log('Nom')
-        
-        if (pellet.id % 28 === 0) {
-          pellet.classList.remove(superPelletClass)
-          pellet.classList.add(pelletEatenClass)
-        } else {
-          pellet.classList.remove(pelletClass)
-          pellet.classList.add(pelletEatenClass)
-        }
-        // * create array for pelets eaten and contain score in attrbute
-        pelletsEatenArray.push(pellet)
-        // ! BUG: score adds every time the setInterval timer runs, and add to itself even when player is 'stuck' against wall. Need a way to evluate score sepetate from the movement timer. i just wanna count the amoung if items that contain the class 'pellet eaten'. need to switch off class once player enters grid, otherwise contion will always ring true. need to make pallet value 0 as soon as entered?
+        //pellet.setAttribute('data-score', 10)
       }
     })
   }
@@ -146,25 +122,51 @@ function init() {
   function addPlayer(position) {
     cells[position].classList.add(playerClass)
   }
-
+  
   // * Remove player from grid on move
   function removePlayer(position) {
     cells[position].classList.remove(playerClass)
   }
 
-  // * Pellet eaten function
-  function handleScore() {
-    //const numberOfPelletsEaten = pelletsEatenArray.length
-    //console.log(numberOfPelletsEaten)
-    pelletsEatenArray.forEach(pellet => {
-      if (pellet.classList.contains('pellet-eaten')) {
-        //console.log('pellet value ->', Number(pellet.getAttribute('data-score'))) 
-        score += Number(pellet.getAttribute('data-score'))
-        //console.log('Score ->', score) 
+  // ! Remove pellets WIP, pellets are removed visually atm
+  function removePellet(playerPosition) {
+    pelletArray.forEach(pellet => {
+      if (playerPosition === Number(pellet.id)) {
+      // console.log('Nom')
+        
+        if (pellet.id % 28 === 0) {
+          pellet.classList.remove(superPelletClass)
+          pellet.classList.add(pelletEatenClass)
+          pellet.setAttribute('data-score', 50)
+          //pellet.removeAttribute('data-score')
+        } else {
+          pellet.classList.remove(pelletClass)
+          pellet.classList.add(pelletEatenClass)
+          pellet.setAttribute('data-score', 10)
+          //pellet.removeAttribute('data-score')
+        }
+        // * create array for pelets eaten and contain score in attrbute
+        pelletsEatenArray.push(pellet)
+        // ! BUG: score adds every time the setInterval timer runs, and add to itself even when player is 'stuck' against wall. Need a way to evluate score sepetate from the movement timer. i just wanna count the amoung if items that contain the class 'pellet eaten'. need to switch off class once player enters grid, otherwise contion will always ring true. need to make pallet value 0 as soon as entered?
       }
-    })   
+    })
+  
+
+
   }
 
+  // * handle Score
+  function handleScore(total) {
+    const eatenPellets = document.getElementsByClassName(pelletEatenClass)
+    const pelletValue = eatenPellets[0].getAttribute('data-score')
+    
+
+    console.log('pellet value ->', pelletValue)
+
+    total += Number(pelletValue)
+    console.log('total', total)
+    return total 
+  }
   
   function handleKeyUp(event) {
     const key = event.keyCode
@@ -197,6 +199,7 @@ function init() {
     const portalRight = 199
 
     removePlayer(playerCurrentPosition)
+    // ! add char
 
     if (playerDirection === 'right' && playerCurrentPosition % width !== width - 1 && !mergedMazeAndGhostHomeArray.includes(playerRelativePositionRight)) {
       playerCurrentPosition++
@@ -223,9 +226,12 @@ function init() {
       // console.log('Player traveled through portal')
     }
 
-    handleScore(score)
-    removePellet(playerCurrentPosition)
     addPlayer(playerCurrentPosition)
+    removePellet(playerCurrentPosition)
+    handleScore(score)
+    // ! remove char
+
+    
   }
   
 
