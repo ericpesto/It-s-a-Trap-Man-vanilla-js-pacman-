8152b301e88a3d43fdf41d0fa086b06b6b18b21d
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-02-23 21:44:43 +0000

    maze update

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,7 @@ function init() {
   const cells = []
   let cell 
   const mazeClass = 'maze-wall'
-  const mazeArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,29,30,39,40,42,43,45,46,47,49,50,52,53,54,56,57,59,60,62,63,65,66,67,69,70,72,73,74,76,77,79,80,87,89,90,92,99,100,102,103,105,114,116,117,119,120,122,123,125,126,127,128,131,132,133,134,136,137,139,140,159,160,161,163,164,165,166,167,172,173,174,175,176,178,179,200,201,203,204,205,206,207,212,213,214,215,216,218,219,220,226,233,239,240,242,243,244,246,248,249,250,251,253,255,256,257,259,260,262,277,279,280,284,286,288,289,290,291,293,295,279,299,300,302,303,304,306,313,315,316,317,319,320,326,328,329,330,331,333,339,340,342,343,344,345,346,353,354,355,356,357,359,360,368,369,370,371,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399] 
+  const mazeArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,29,30,39,40,42,43,45,46,47,49,50,52,53,54,56,57,59,60,62,63,65,66,67,69,70,72,73,74,76,77,79,80,87,89,90,92,99,100,102,103,105,114,116,117,119,120,122,123,125,126,127,128,131,132,133,134,136,137,139,140,159,160,161,162,163,165,166,167,172,173,174,176,177,178,179,200,201,202,203,205,206,207,212,213,214,216,218,217,219,220,226,233,239,240,242,243,244,246,248,249,250,251,253,255,256,257,259,260,262,277,279,280,284,286,288,289,290,291,293,295,279,299,300,302,303,304,306,313,315,316,317,319,320,326,328,329,330,331,333,339,340,342,343,344,345,346,353,354,355,356,357,359,360,368,369,370,371,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399] 
   const ghostHomeArray = [169,170,189,190,209,210]
   const ghostHomeClass = 'ghost-home'
   const portalLocations = [180,199]
@@ -37,9 +37,9 @@ function init() {
   // * Player object
   const player = {
     name: 'Dave', // ! make it so user can add name to personalise experience
-    startPosition: 21,
-    currentPosition: 21,
-    direction: 'up',
+    startPosition: 361,
+    currentPosition: 361,
+    direction: 'down',
     class: 'player',
     lives: 3,
     positionX: function() {
@@ -147,9 +147,9 @@ function init() {
         cells[char.currentPosition].classList.remove(char.className)
         char.currentPosition += direction
 
-        const charCoordinates = [char.positionX(), char.positionY()]
+        const charCoordinates = [char.positionX(), char.positionY() - 40]
         //console.log(charCoordinates)
-        const targetCoordinates = [player.positionX() - 7, player.positionY() / 2]
+        const targetCoordinates = [player.positionX() - 10, player.positionY()]
         const charNextMoveCoordinates =  getNextMoveCoordinates(char.currentPosition + direction)
         //console.log('next tile ->', charNextMoveCoordinates)
 
@@ -233,7 +233,7 @@ function init() {
 
         const noaCoordinates = [noa.positionX(), noa.positionY()]
         //console.log(charCoordinates)
-        const targetCoordinates = [player.positionX() - 14 , player.positionY() * 2]
+        const targetCoordinates = [player.positionX() - 20 , player.positionY() - 40]
         const noaNextMoveCoordinates =  getNextMoveCoordinates(noa.currentPosition + direction)
         //console.log('next tile ->', charNextMoveCoordinates)
 
@@ -317,9 +317,9 @@ function init() {
         cells[jos.currentPosition].classList.remove(jos.className)
         jos.currentPosition += direction
 
-        const josCoordinates = [jos.positionX() - 7, jos.positionY() - 10]
+        const josCoordinates = [jos.positionX() - 7, jos.positionY() - 40]
         //console.log(charCoordinates)
-        const targetCoordinates = [player.positionX() - 7, player.positionY() - width]
+        const targetCoordinates = [player.positionX() + 14, player.positionY()]
         const josNextMoveCoordinates =  getNextMoveCoordinates(jos.currentPosition + direction)
         //console.log('next tile ->', charNextMoveCoordinates)
 
