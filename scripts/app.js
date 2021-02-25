6fd3d98d6721bb9f086b8477cfdd5784409e4dac
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-02-25 12:28:23 +0000

    updated map and ghost pen

--- scripts/app.js
+++ scripts/app.js
@@ -29,8 +29,8 @@ function init() {
   const cells = []
   let cell 
   const mazeClass = 'maze-wall'
-  const mazeArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,29,30,39,40,42,43,45,46,47,49,50,52,53,54,56,57,59,60,62,63,65,66,67,69,70,72,73,74,76,77,79,80,87,89,90,92,99,100,102,103,105,114,116,117,119,120,122,123,125,126,127,128,131,132,133,134,136,137,139,140,159,160,161,162,163,165,166,167,172,173,174,176,177,178,179,200,201,202,203,205,206,207,212,213,214,216,218,217,219,220,226,233,239,240,242,243,244,246,248,249,250,251,253,255,256,257,259,260,262,277,279,280,284,286,288,289,290,291,293,295,279,299,300,302,303,304,306,313,315,316,317,319,320,326,328,329,330,331,333,339,340,342,343,344,345,346,353,354,355,356,357,359,360,368,369,370,371,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399] 
-  const ghostHomeArray = [169,170,189,190,209,210]
+  const mazeArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,29,30,39,40,42,43,45,46,47,49,50,52,53,54,56,57,59,60,62,63,65,66,67,69,70,72,73,74,76,77,79,80,87,89,90,92,99,100,102,103,105,114,116,117,119,120,122,123,125,126,127,128,131,132,133,134,136,137,139,140,159,160,161,162,163,165,166,168,171,173,174,176,177,178,179,188,191,200,201,202,203,205,206,213,214,216,218,217,219,220,226,228,229,230,231,233,239,240,242,243,244,246,248,249,250,251,253,255,256,257,259,260,262,277,279,280,284,286,288,289,290,291,293,295,279,299,300,302,303,304,306,313,315,316,317,319,320,326,328,329,330,331,333,339,340,342,343,344,345,346,353,354,355,356,357,359,360,368,369,370,371,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399] 
+  const ghostHomeArray = [169,170,189,190]
   const ghostHomeClass = 'ghost-home'
   const portalLocations = [180,199]
   const portalClass = 'portal'
@@ -118,8 +118,8 @@ function init() {
   const char = {
     name: 'Char',
     className: 'char',
-    startingPosition: ghostHomeArray[4],
-    currentPosition: ghostHomeArray[4],
+    startingPosition: ghostHomeArray[0],
+    currentPosition: ghostHomeArray[0],
     speed: 270,
     positionX: function() {
       return char.currentPosition % width
@@ -241,8 +241,8 @@ function init() {
   const jos = {
     name: 'Jos',
     className: 'jos',
-    startingPosition: ghostHomeArray[0],
-    currentPosition: ghostHomeArray[0],
+    startingPosition: ghostHomeArray[2],
+    currentPosition: ghostHomeArray[2],
     speed: 320,
     positionX: function() {
       return jos.currentPosition % width
@@ -303,8 +303,8 @@ function init() {
   const guy = {
     name: 'Guy',
     className: 'guy',
-    startingPosition: ghostHomeArray[5],
-    currentPosition: ghostHomeArray[5],
+    startingPosition: ghostHomeArray[3],
+    currentPosition: ghostHomeArray[3],
     speed: 400,
     add(position) {
       //console.log('guy added')
